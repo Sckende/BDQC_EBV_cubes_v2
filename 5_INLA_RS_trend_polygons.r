bbea3d21324a3794a3352e281f0df500a58a122b
Claire <clrccljuhasz@gmail.com>
2024-07-18 10:00:01 -0400

    latest changes on trend polygons

--- 5_INLA_RS_trend_polygons.r
+++ 5_INLA_RS_trend_polygons.r
@@ -3,27 +3,42 @@
 library(terra)
 library(sf)
 library(mapview)
+library(RCurl)
+library(rmapshaper)
 
-list.files("/home/claire/BDQC-GEOBON/data/QUEBEC_regions/sf_CERQ_SHP/")
 # CERQ avec 5 nibveaux
 # Ecozones
+ecoz <- st_read("https://object-arbutus.cloud.computecanada.ca/bq-io/acer/qc_polygons/sf_eco_poly/qc_ecozones.gpkg")
+ecoz <- ms_simplify(ecoz)
+
 # Ecoregions
+ecor <- st_read("https://object-arbutus.cloud.computecanada.ca/bq-io/acer/qc_polygons/sf_eco_poly/qc_ecoregions.gpkg")
+
 # Ecoprovinces
 # Ecodistricts
+species <- read.table("INLA_species_list_final.txt", h = F)
+ <- data.frame()
+summ_rs <- data.frame(matrix(ncol = 4, nrow = 0))
+x <- c("year", "poly_name", "poly_ID", "spe_rich")
+colnames(summ_rs) <- x
 
-# uniquement pour Yoga
-local_path <- "/home/claire/BDQC-GEOBON/data/QUEBEC_regions/"
-ecoZo <- st_read(paste0(local_path, "/Ecozones")) # 25 polygones
-ecoPro <- st_read(paste0(local_path, "/Ecoprovinces")) # 68 polygones
-ecoReg <- st_read(paste0(local_path, "/Ecoregions")) # 218 polygones
-ecoDis <- st_read(paste0(local_path, "/Ecodistricts")) # 1025 polygones
-mapview(list(ecoZo, ecoPro, ecoReg, ecoDis))
+for (year in 1992:2017) {
+    path_list <- c()
+    spe_rich <- 0
+    for(i in 1:length(species[,1])){
+        path <- paste0("https://object-arbutus.cloud.computecanada.ca/bq-io/acer/TdeB_benchmark_SDM/oiseaux-nicheurs-qc/", species[i, 1], "_range_", year, ".tif")
+        if(url.exists(path)){
+            path_list <- c(path_list, path)
+        }
+    stak <- rast(path_list)
+    for(j in 1:nrow(ecoz)){
+        test <- terra::crop(stak, ecoz[j,])
+        sum(colSums(test[, -1]) > 0)
+    }
+    
 
-qc <- st_union(st_read(paste0(local_path, "/sf_CERQ_SHP/QUEBEC_CR_NIV_01.gpkg")))
-qc_ll <- st_transform(qc, st_crs(ecoZo))
+    }
 
-# Crop canadian shapefile with Qc
-can_spat <- list(ecoZo, ecoPro, ecoReg, ecoDis)
-qc_eco <- lapply(can_spat, function(x) {
-    inter <- st_intersection(x, qc_ll)
-})
+}
+
+mapview(ecoz[7,])
